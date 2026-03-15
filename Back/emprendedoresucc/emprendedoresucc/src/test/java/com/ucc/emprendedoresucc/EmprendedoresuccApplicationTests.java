c47d48cb246e606fef74158a0824b54e42ce411e
SebasCasta05 <sebastianccrojas@gmail.com>
2026-03-14 23:20:13 -0500

    spring boot

---
+++ Back/emprendedoresucc/emprendedoresucc/src/test/java/com/ucc/emprendedoresucc/EmprendedoresuccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucc.emprendedoresucc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprendedoresuccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
